0419595c29cdf1fe55036430e081d8a0c917a121
Sirgey-Pats <sirgey.patsora@gmail.com>
2021-02-21 00:22:51 +0200

    js header

---
+++ js/header.js
@@ -0,0 +1,47 @@
+$('.header').addClass('fix')
+
+let scrollPos = 0;
+window.addEventListener('scroll', function () {
+	let y = scrollY;
+	//console.log('y = ' + y);
+	let cnt_w = $('.hero').innerHeight();
+	//console.log(cnt_w);
+	/*
+			if (y >= cnt_w) {
+				$('.header').addClass('fix')
+			}
+			else if (y < cnt_w) {
+				$('.header').removeClass('fix')
+			}*/
+	let up = $(window).scrollTop();
+	//console.log('up = ' + up);
+	//console.log('scrollPos = ' + scrollPos);
+	//опа
+	if (up <= scrollPos) {
+		$('.header').addClass('fix')
+	} else if (up => scrollPos) {
+		$('.header').removeClass('fix')
+	}
+	scrollPos = up;
+	if (y >= 2) {
+		let headerHeight = $('.header').css({ 'height': '50px', 'background-color': '#ebeaf9' });
+	} else if (y < 2 && window.innerWidth > 840) {
+		let headerHeight = $('.header').css({ 'height': '180px', 'background-color': '#ebeaf900' });
+	}
+
+});
+$(window).on("resize", function () {
+	if (window.innerWidth < 840) {
+		let headerHeight = $('.header').css({ 'height': '50px', 'background-color': '#ebeaf9' });
+	} else if (window.innerWidth > 840) {
+		let headerHeight = $('.header').css({ 'height': '180px', 'background-color': '#ebeaf900' });
+	}
+})
+
+
+onmousemove = function (e) {
+	console.log("mouse location:", e.clientX, e.clientY)
+	if (e.clientY <= 30) {
+		$('.header').addClass('fix')
+	}
+}
--- js/script.js
+++ js/script.js
@@ -2,54 +2,11 @@ jQuery('document').ready(function () {
 	$('.header__burger').on('click', function (event) {
 		$('.header__burger,.header, .header__menu, body').toggleClass('active');
 	})
-
-	$('.header').addClass('fix')
-
-	let scrollPos = 0;
-	window.addEventListener('scroll', function () {
-		let y = scrollY;
-		//console.log('y = ' + y);
-		let cnt_w = $('.hero').innerHeight();
-		//console.log(cnt_w);
-		/*
-				if (y >= cnt_w) {
-					$('.header').addClass('fix')
-				}
-				else if (y < cnt_w) {
-					$('.header').removeClass('fix')
-				}*/
-		let up = $(window).scrollTop();
-		//console.log('up = ' + up);
-		//console.log('scrollPos = ' + scrollPos);
-		//опа
-		if (up <= scrollPos) {
-			$('.header').addClass('fix')
-		} else if (up => scrollPos) {
-			$('.header').removeClass('fix')
-		}
-		scrollPos = up;
-		if (y >= 2) {
-			let headerHeight = $('.header').css({ 'height': '50px', 'background-color': '#ebeaf9' });
-		} else if (y < 2 && window.innerWidth > 840) {
-			let headerHeight = $('.header').css({ 'height': '180px', 'background-color': '#ebeaf900' });
-		}
-
-	});
-	$(window).on("resize", function () {
-		if (window.innerWidth < 840) {
-			let headerHeight = $('.header').css({ 'height': '50px', 'background-color': '#ebeaf9' });
-		} else if (window.innerWidth > 840) {
-			let headerHeight = $('.header').css({ 'height': '180px', 'background-color': '#ebeaf900' });
-		}
-	})
+	import('/js/header.js');
 
 
-	onmousemove = function (e) {
-		console.log("mouse location:", e.clientX, e.clientY)
-		if (e.clientY <= 30) {
-			$('.header').addClass('fix')
-		}
-	}
+
+
 
 
 })
