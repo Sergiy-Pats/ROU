6890a33281f9131c07627ff8b431c05c23626688
Sirgey-Pats <sirgey.patsora@gmail.com>
2021-02-20 23:30:12 +0200

    New

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
 jQuery('document').ready(function () {
 	$('.header__burger').on('click', function (event) {
-		$('.header__burger, .header__menu, body').toggleClass('active');
+		$('.header__burger,.header, .header__menu, body').toggleClass('active');
 	})
+
+	$('.header').addClass('fix')
+
+	let scrollPos = 0;
+	window.addEventListener('scroll', function () {
+		let y = scrollY;
+		//console.log('y = ' + y);
+		let cnt_w = $('.hero').innerHeight();
+		//console.log(cnt_w);
+		/*
+				if (y >= cnt_w) {
+					$('.header').addClass('fix')
+				}
+				else if (y < cnt_w) {
+					$('.header').removeClass('fix')
+				}*/
+		let up = $(window).scrollTop();
+		//console.log('up = ' + up);
+		//console.log('scrollPos = ' + scrollPos);
+		if (up <= scrollPos) {
+			$('.header').addClass('fix')
+		} else if (up => scrollPos) {
+			$('.header').removeClass('fix')
+		}
+		scrollPos = up;
+		if (y >= 2) {
+			let headerHeight = $('.header').css({ 'height': '50px', 'background-color': '#ebeaf9' });
+		} else {
+			let headerHeight = $('.header').css({ 'height': '180px', 'background-color': '#ebeaf900' });
+		}
+	});
+
+
+	onmousemove = function (e) {
+		console.log("mouse location:", e.clientX, e.clientY)
+		if (e.clientY <= 30) {
+			$('.header').addClass('fix')
+		}
+	}
+
+
 })
 
