c8fd9db5fcc3097b446c8dfc93b5316e90501a81
Sirgey-Pats <sirgey.patsora@gmail.com>
2021-02-21 17:44:07 +0200

    123

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ jQuery('document').ready(function () {
 	$('.header__burger').on('click', function (event) {
 		$('.header__burger,.header, .header__menu, body').toggleClass('active');
 	})
-	import('/js/header.js');
+	import('./header.js');
 
 
 
