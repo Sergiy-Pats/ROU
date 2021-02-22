5a01e2e1db28d351dd73c2c7ae775f0b46784126
Sirgey-Pats <sirgey.patsora@gmail.com>
2021-02-22 17:21:08 +0200

    123

--- js/script.js
+++ js/script.js
@@ -3,30 +3,23 @@ jQuery('document').ready(function () {
 		$('.header__burger,.header, .header__menu, body').toggleClass('active');
 	})
 	import('../js/header.js');
-
-
-
-	//if (window.innerWidth > 1180) {
-
 	let scrollPos_1 = 0;
 	window.addEventListener('scroll', function (e) {
-		console.log(window.innerWidth);
+		//console.log(window.innerWidth);
 
 		let anim = scrollY;
-		console.log('anim = ' + anim);
+		//console.log('anim = ' + anim);
 		let top = $('.intro').offset().top;
 
-		console.log('top = ' + top);
+		//console.log('top = ' + top);
 
 
 		let lft = anim - top - 500 + 345;
-		//parseInt($('.item-intro-1').css('left'));
-		console.log("left = " + lft);
+		//console.log("left = " + lft);
 
 
-		//if (anim > scrollPos_1) {//scroll down
 
-		if (/*anim >= top - 500 && */anim + -150 < top && window.innerWidth > 1180) {
+		if (anim + -150 < top && window.innerWidth > 1180) {
 			$('.item-intro-1').css("transform", 'translateX(' + lft + 'px)');
 
 			$('.item-intro-3').css("transform", 'translateX(' + -lft + 'px)');
@@ -38,28 +31,21 @@ jQuery('document').ready(function () {
 		}
 
 
-		//}
-		/*
-		else if (anim < scrollPos_1) {//scroll up
-			if (anim >= 1050 && anim < 1550) {
-				$('.item-intro-1').css('left', lft - 460 + "px")
-					;
-			}
-		}*/
-
-
 		scrollPos_1 = anim;
 	})
 
-	//}
+	//-------------------------------------------------------------
+	//отмена стандартного действия при клике на кнопку (а href)
+	$('a').on('click', function (event) {
+		console.log('Мы отменили действие');
 
+		event.preventDefault();
 
+	}, false);
+	//-------------------------------
 
 
 	import('../js/slider.js');
-
-	// top = растояние с верха до блока + верхний отступ  --- точка отчета
-	// left = scrollY - top - 500
 
 })
 
