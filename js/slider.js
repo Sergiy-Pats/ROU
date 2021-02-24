7c056674b635411772addbdddbf3e5b210229c83
Sirgey-Pats <sirgey.patsora@gmail.com>
2021-02-25 00:56:31 +0200

    123123123123123

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,8 @@
 
 // При старте ссайта с разширением меньше 768 слайдер активен
-if (window.innerWidth <= 768) {
+//if (window.innerWidth <= 768) {
+function slider_1() {
 	$('.slider').slick({
-		slidesToShow: 1,
 		infinite: true,
 		autoplay: true,
 		autoplaySpeed: 6200,
@@ -13,60 +13,62 @@ if (window.innerWidth <= 768) {
 		dots: true,
 		arrows: false,
 		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: 'unslick'
+			},
+		]
 	});
+}
+function slider_2() {
 
 	$('.slider-2').slick({
-		slidesToShow: 1,
 		infinite: true,
 		autoplay: true,
+		slidesToShow: 2,
 		autoplaySpeed: 6200,
-		pauseOnHover: true,
-		pauseOnFocus: true,
-		pauseOnDotHover: false,
 		speed: 800,
 		arrows: false,
 		mobileFirst: false,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		responsive: [
+
+			{
+				breakpoint: 10000,
+				settings: 'unslick'
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 540,
+				settings: {
+					slidesToShow: 1,
+				}
+			},
+		]
 
 	});
-
 }
+if ($(window).width() < 767) {
+	slider_1();
+	slider_2();
+}
+//}
 //динамическое вкл\выкл слайдера
+
 $(window).on('resize', function () {
 	//console.log('width = ' + window.innerWidth);
-
-	if (window.innerWidth <= 768) {
-
-		$('.slider-2').slick({
-			slidesToShow: 1,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 6200,
-			pauseOnHover: true,
-			pauseOnFocus: true,
-			pauseOnDotHover: false,
-			speed: 800,
-			arrows: false,
-			mobileFirst: false,
-		});
-
-		$('.slider').slick({
-			slidesToShow: 1,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 3200,
-			pauseOnHover: true,
-			pauseOnFocus: true,
-			pauseOnDotHover: false,
-			speed: 800,
-			arrows: false,
-			dots: true,
-			mobileFirst: true,
-		});
-
-	} else if (window.innerWidth > 768) {
-		$('.slider').slick('unslick');
-		$('.slider-2').slick('unslick');
-
+	let $windowWidth = $('windwow').width();
+	if ($windowWidth <= 768 && $('.slider-2').hasClass('slick-initialized') && $('.slider').hasClass('slick-initialized')) {
+	}
+	else {
+		slider_1();
+		slider_2();
 	}
 })
-
